bbe278537fdfa5d94fe63f1b1b43da20207b6bd1
Fernando Antônio Ferreira Ibrahim <80763509+FernandoIbrahim@users.noreply.github.com>
2025-06-17 10:22:13 -0300

    feat: auth_service login method

--- code/mobile/lib/modules/common/services/auth_service.dart
+++ code/mobile/lib/modules/common/services/auth_service.dart
@@ -1,10 +1,15 @@
 import 'dart:convert';
+import 'package:dio/dio.dart';
 import 'package:flutter/services.dart';
+import 'package:mobile/modules/common/dio.dart';
 
 /// A service that handles authentication operations.
 ///
 /// This service includes methods to log in users using mock data.
 class AuthService {
+
+  final dioClient = DioClient().dio;
+
   Future<Map<String, dynamic>> _loadMockData() async {
     final String response = await rootBundle.loadString('assets/mock/data.json');
     return json.decode(response);
@@ -21,4 +26,28 @@ class AuthService {
     }
     return null;
   }
+
+  Future<String?> login2(String email, String password) async {
+    try {
+      final response = await dioClient.post(
+        "/user/login",
+        data: {
+          "email": email,
+          "password": password,
+        },
+      );
+
+      if (response.data != null && response.data['token'] != null) {
+        return response.data['token']; // pega o token da resposta
+      }
+
+      return null;
+
+    } catch (e) {
+      print('Erro ao fazer login: $e');
+      return null;
+    }
+
+  }
+
 }
